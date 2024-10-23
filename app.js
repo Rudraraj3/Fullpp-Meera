6d0768134a84c3f545b62db2281223bee1a93a7b
AmeenXnt <ameenserbots@gmail.com>
2024-10-24 00:29:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Meera')));
 
 // Use the profile router for handling WhatsApp profile updates
 app.use('/api/profile', profileRouter);
